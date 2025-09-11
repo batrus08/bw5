a7d1138c28d1054408e71a88c7afda98d125d24d
batrus08 <trtcara@gmail.com>
2025-09-11 20:06:01 +0700

    Add initial products table migration

--- prisma/migrations/20240918120000_sheet_fifo_variants/migration.sql
+++ prisma/migrations/20240918120000_sheet_fifo_variants/migration.sql
@@ -1,14 +1,3 @@
--- AlterEnum
--- This migration adds more than one value to an enum.
--- With PostgreSQL versions 11 and earlier, this is not possible
--- in a single migration. This can be worked around by creating
--- multiple migrations, each migration adding only one value to
--- the enum.
-
-
-ALTER TYPE "DeliveryMode" ADD VALUE 'USERPASS';
-ALTER TYPE "DeliveryMode" ADD VALUE 'INVITE_ONLY';
-
 -- AlterTable
 ALTER TABLE "accounts" ADD COLUMN     "fifo_order" BIGINT NOT NULL,
 ADD COLUMN     "max_usage" INTEGER NOT NULL DEFAULT 1,
